a58b440852f3354658157d84ad8c81b9488154ae
真間雄己 <coro@korono-MacBook-Air.local>
2019-04-01 20:37:03 +0900

    微修正

--- upallnight/js/main.js
+++ upallnight/js/main.js
@@ -18,28 +18,72 @@ $(function() {
       }, duration);
     });
 
-    /**
-     * グローバルナビゲーションをステッキーヘッダーにする
-     **/
-     $("#gnav").each(function() {
-       let $window = $(window);
-       let $header = $(this);
+  /**
+   * グローバルナビゲーションをステッキーヘッダーにする
+   **/
+  $("#gnav").each(function() {
+    let $window = $(window);
+    let $header = $(this);
 
-       // ヘッダーのデフォルトの位置を取得
-       let headerOffsetTop = $header.offset().top;
+    // ヘッダーのデフォルトの位置を取得
+    let headerOffsetTop = $header.offset().top;
 
-       // ウィンドウのスクロールイベントを監視し、スクロールを検知した際に処理を実行する
-       $window.on("scroll",  $.throttle(1000 / 15, function () {
-         // ウィンドウのスクロール量とヘッダーのデフォルトの位置を比較する
-         // スクロール量＞ヘッダーのデフォルトの位置であればstickyクラスを付与する
-         // スクロール量＝＜ヘッダーのデフォルトの位置であればstickyクラスを削除する
-         if ($window.scrollTop() > headerOffsetTop) {
-           $header.addClass("sticky");
-         } else {
-           $header.removeClass("sticky");
-         }
-       }));
-       // ウィンドウのスクロールイベントを発生させる。
-      $window.trigger("scroll");
-     });
+    // ウィンドウのスクロールイベントを監視し、スクロールを検知した際に処理を実行する
+    $window.on("scroll", $.throttle(1000 / 15, function() {
+      // ウィンドウのスクロール量とヘッダーのデフォルトの位置を比較する
+      // スクロール量＞ヘッダーのデフォルトの位置であればstickyクラスを付与する
+      // スクロール量＝＜ヘッダーのデフォルトの位置であればstickyクラスを削除する
+      if ($window.scrollTop() > headerOffsetTop) {
+        $header.addClass("sticky");
+      } else {
+        $header.removeClass("sticky");
+      }
+    }));
+    // ウィンドウのスクロールイベントを発生させる。
+    $window.trigger("scroll");
+  });
+
+
+
+  $("#mainContents").css("display", "none");
+  $("#opening2").css("display", "none");
+  setTimeout(function() {
+    $('#openning1').fadeOut("slow");
+  }, 3000);
+
+  setInterval(function() {
+    $('#openning1').animate({
+      backgroundPositionY: '+=' + 0.1 + '%'
+    }, 5);
+  }, 10);
+
+  setTimeout(function() {
+    $("#opText1").text("「 ねえ、 「Man in The Mirror」　って知ってる？ 」");
+  }, 0);
+
+
+  setTimeout(function() {
+    $("#opText2").html("「 日没後、數十分だけ体感できる、<br>空が魔法のような薄明かりに包まれる時間帯を<br>「Man in The Mirror」って言うんだって 」");
+  }, 5000);
+
+
+  setInterval(function() {
+    $('#openning2').animate({
+      backgroundPositionY: '+=' + 0.1 + '%'
+    }, 5);
+  }, 10);
+
+  setTimeout(function() {
+    $("#openning2").fadeIn();
+  }, 4000);
+
+  setTimeout(function() {
+    $('#openning2').fadeOut("slow");
+  }, 7000);
+
+
+  setTimeout(function() {
+    $("#mainContents").fadeIn();
+  }, 8000);
+
 });
