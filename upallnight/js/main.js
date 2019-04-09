59eef8755ee26301c9e5ac9478ff83bf3ba1e683
真間雄己 <coro@korono-MacBook-Air.local>
2019-04-09 21:48:49 +0900

    OP強化・ヘッダー追加・フッダー強化・deteil強化

--- upallnight/js/main.js
+++ upallnight/js/main.js
@@ -8,18 +8,18 @@ $(function() {
   let yourName = escapeHtml(localStorage.name);
 
   function escapeHtml(str) {
-    if(str == null || str == ""){
+    if (str == null || str == "") {
       return "ひろし";
     }
-	    str = str.replace(/&/g, '&amp;');
-	    str = str.replace(/</g, '&lt;');
-	    str = str.replace(/>/g, '&gt;');
-	    str = str.replace(/"/g, '&quot;');
-	    str = str.replace(/'/g, '&#39;');
-	    return str;
-	}
+    str = str.replace(/&/g, '&amp;');
+    str = str.replace(/</g, '&lt;');
+    str = str.replace(/>/g, '&gt;');
+    str = str.replace(/"/g, '&quot;');
+    str = str.replace(/'/g, '&#39;');
+    return str;
+  }
 
-$("#yourName").text(yourName);
+  $("#yourName").text(yourName);
 
   // キャラクター表示処理
   function showCharacter(index) {
@@ -31,7 +31,7 @@ $("#yourName").text(yourName);
             "alt": "主人公の画像"
           });
         $(setId + " #selectedCharacter .character").find("p")
-          .html("・"+yourName+"<br>高校2年生<br>人に流されやすい性格<br>人に言われるがまま行動することが当たり前になっている<br>BL漫画が好き<br><br>");
+          .html("・" + yourName + "<br>高校2年生<br>人に流されやすい性格<br>人に言われるがまま行動することが<wbr>当たり前になっている<br>BL漫画が好き<br><br>");
         break;
       case 1:
         $(setId + " #selectedCharacter .character").find("img")
@@ -40,7 +40,7 @@ $("#yourName").text(yourName);
             "alt": "主人公の画像"
           });
         $(setId + " #selectedCharacter .character").find("p")
-          .html("・鏡の中の自分<br>突然鏡の中に現れ、自立して話しかけてくる<br>嫌味ったらしい性格<br>"+yourName+"に苦言を呈してくるが、<br>心を読んでいるかのような、的確な指摘をしてくる<br><br>");
+          .html("・鏡の中の自分<br>突然鏡の中に現れ、自立して話しかけてくる<br>嫌味ったらしい性格<br>" + yourName + "に苦言を呈してくるが、<br>心を読んでいるかのような、<wbr>的確な指摘をしてくる<br><br>");
         break;
       case 2:
         $(setId + " #selectedCharacter .character").find("img")
@@ -49,7 +49,7 @@ $("#yourName").text(yourName);
             "alt": "主人公の画像"
           });
         $(setId + " #selectedCharacter .character").find("p")
-          .html("・みき<br>"+yourName+"が気になっている子<br>クラス一の美少女<br>大人しい性格だが、自分の意思をしっかり持っている<br>");
+          .html("・みき<br>" + yourName + "が気になっている子<br>クラス一の美少女<br>大人しい性格だが、<wbr>自分の意思をしっかり持っている<br>");
         break;
       case 3:
         $(setId + " #selectedCharacter .character").find("img")
@@ -58,7 +58,7 @@ $("#yourName").text(yourName);
             "alt": "主人公の画像"
           });
         $(setId + " #selectedCharacter .character").find("p")
-          .html("・キテラ<br>"+yourName+"の友人<br>長身・でぶ<br>自分の欲望に忠実な男<br>ある日、近所の祠を不注意で壊してから、<br>不審な行動に出るように…<br>");
+          .html("・キテラ<br>" + yourName + "の友人<br>自分の欲望に忠実な男<br>ある日、近所の祠を不注意で壊してから、<br>不審な行動に出るように…<br>");
         break;
     }
   }
@@ -67,6 +67,7 @@ $("#yourName").text(yourName);
   $(setId + ' #cnav ul li').click(function() {
     let connectCont = $(setId + ' #cnav ul li').index(this);
     showCharacter(connectCont);
+    showCharacterAnimate();
     $(this).addClass('active');
     $(this).siblings().removeClass('active');
   });
@@ -110,28 +111,33 @@ $("#yourName").text(yourName);
 
   setTimeout(function() {
     $('#openning1').fadeOut("slow");
-  }, 5000);
+  }, 6000);
 
   setTimeout(function() {
     $("#opText1").text("「こんにちは、" + yourName + "くん」");
+    opFadeIn($("#opText1"));
   }, 0);
 
   setTimeout(function() {
     $("#opText1").text("突然、鏡の中の自分に話しかけられた");
+    opFadeIn($("#opText1"));
   }, 2000);
 
   setTimeout(function() {
     $("#opText1").text("高校2年の夏、その転機は突然訪れた…");
+    opFadeIn($("#opText1"));
   }, 4000);
 
 
   setTimeout(function() {
     $("#opText2").html("変わるのは自分だ<br>変えるのは自分だ");
-  }, 6000);
+    opFadeIn($("#opText2"));
+  }, 7000);
 
   setTimeout(function() {
-    $("#opText2").html("全てはこれから始まる….");
-  }, 9000);
+    $("#opText2").html("全てはこれから始まる…");
+    opFadeIn($("#opText2"));
+  }, 10000);
 
   let timer2 = setInterval(function() {
     $('#openning2').animate({
@@ -142,22 +148,22 @@ $("#yourName").text(yourName);
 
   setTimeout(function() {
     $("#openning2").fadeIn();
-  }, 3900);
+  }, 5000);
 
 
   setTimeout(function() {
     $('#openning2').fadeOut("slow");
     clearInterval(timer2);
-  }, 7000);
+  }, 10500);
 
 
   setTimeout(function() {
     $("#mainContents").fadeIn();
-  }, 12000);
+  }, 14000);
 
 
   // Top Pageアイコン表示処理
-  var topBtn = $('#page-top');
+  let topBtn = $('#page-top');
   topBtn.hide();
   //スクロールが100に達したらボタン表示
   $(window).scroll(function() {
@@ -176,5 +182,48 @@ $("#yourName").text(yourName);
     return false;
   });
 
+  //コンテンツのアニメーション
+  let target = $('.inviewAction');
+  target.css('opacity', '0');
+  target.on('inview', function(event, isInView) {
+    //表示領域に入った時にスライドイン
+    if (isInView) {
+      $(this).animate({
+        opacity: 1
+      }, 1000).addClass('slideInLeft');
+    }
+  });
+
+  $('header').on('inview', function(event, isInView) {
+    //表示領域に入った時にスライドイン
+    if (isInView) {
+
+      setTimeout(function() {
+        $("#mainMessage1").addClass('fadeInUp');
+      }, 600);
+
+      setTimeout(function() {
+        $("#mainMessage2").addClass('fadeInUp');
+      }, 1500);
+
+    }
+  });
+
+
+
+function opFadeIn(obj){
+  obj.css('opacity', 0).animate({
+    opacity: 1
+  }, 600);
+}
+
+  //キャラクター表示時のアニメーション
+  function showCharacterAnimate() {
+    $('.character').removeClass('fadein');
+    $('.character').css('opacity', 0);
+    $('.character').stop(true).animate({
+      opacity: 1
+    }, 1000).addClass('fadeIn');
+  }
 
 });
